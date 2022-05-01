4a11d31339fb7c1b20f53acdb39959f03f812c80
Chonathon <79057519+Chonathon@users.noreply.github.com>
2022-05-01 01:14:36 -0400

    Added Timed Spawn Script

--- PixCell/Assets/Scripts/timedSpawn.cs
+++ PixCell/Assets/Scripts/timedSpawn.cs
@@ -3,16 +3,33 @@ using UnityEngine;
 public class timedSpawn : MonoBehaviour
 {
 
-    public GameObject RBC;
+    public GameObject objectToSpawn;
+
+    public float timeToSpawn;
+    private float currentTimeToSpawn;
+
     // Start is called before the first frame update
     private void Start()
     {
-        Instantiate(RBC, new Vector3(0, 0, 0), Quaternion.identity); 
+        // Instantiate(objectToSpawn, transform.position, Quaternion.identity); 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        if(currentTimeToSpawn > 0)
+        {
+            currentTimeToSpawn -= Time.deltaTime;
+        }
+        else
+        {
+            SpawnObject();
+            currentTimeToSpawn = timeToSpawn;
+        }
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(objectToSpawn, transform.position, Quaternion.identity); 
     }
 }
